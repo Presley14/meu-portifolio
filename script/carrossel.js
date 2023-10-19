fbcb1eb08c2f35b88409c503cd2497f0ef00cc0d
elvis <flashatemporal@gmail.com>
2023-10-18 22:38:25 -0300

    carrossel mobile feito

---
+++ script/carrossel.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const carrossel = document.querySelector(".carrossel");
+    const cards = document.querySelectorAll(".card_projeto");
+    let cardIndex = Math.floor(cards.length / 2);
+  
+    function atualizarCarrossel() {
+      carrossel.style.transform = `translateX(-${cardIndex * 100}%)`;
+      cards.forEach((card, index) => {
+        card.style.filter = index === cardIndex ? "none" : "blur(5px)";
+        card.classList.toggle("atual", index === cardIndex);
+      });
+      atualizarCarrossel()
+    }
+  
+    function moverCarrossel(direcao) {
+      if (direcao === "prev" && cardIndex > 0) {
+        cardIndex--;
+      } else if (direcao === "next" && cardIndex < cards.length - 1) {
+        cardIndex++;
+      }
+      atualizarCarrossel();
+    }
+  
+    document.getElementById("anterior").addEventListener("click", () => moverCarrossel("prev"));
+    document.getElementById("proximo").addEventListener("click", () => moverCarrossel("next"));
+  
+    atualizarCarrossel();
+  });
---
+++ script/menuHamburguer.js
@@ -0,0 +1,21 @@
+
+function menuClick(){
+
+    const menu = document.querySelector("#menu")
+    const img_menu = document.querySelector("#img_menu")
+    const img_x = document.querySelector("#img_x")
+
+    if(menu.style.display === "none" || menu.style.display === ""){
+        menu.style.display = "block"
+        img_menu.style.display = "none"
+        img_x.style.display = "block"
+    } else{
+        menu.style.display = "none"
+        img_menu.style.display = "block"
+        img_x.style.display = "none"
+    }
+}
+
+
+
+
