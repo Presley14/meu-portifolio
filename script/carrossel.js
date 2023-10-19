4dfe2f72586c32131f9180d960a943b998e6ed63
elvis <flashatemporal@gmail.com>
2023-10-19 19:29:53 -0300

    99% finalizado

--- script/carrossel.js
+++ script/carrossel.js
@@ -1,14 +1,21 @@
+
+
 document.addEventListener("DOMContentLoaded", function() {
-    const carrossel = document.querySelector(".carrossel");
-    const cards = document.querySelectorAll(".card_projeto");
-    let cardIndex = Math.floor(cards.length / 2);
+    const carrossel = document.querySelector(".carrossel")
+
+    const cards = document.querySelectorAll(".card_projeto")
+    let cardIndex = Math.floor(cards.length / 2)
   
     function atualizarCarrossel() {
-      carrossel.style.transform = `translateX(-${cardIndex * 100}%)`;
-      cards.forEach((card, index) => {
-        card.style.filter = index === cardIndex ? "none" : "blur(5px)";
-        card.classList.toggle("atual", index === cardIndex);
-      });
+        carrossel.style.transform = `translateX(-${cardIndex * 100}%)`
+        
+                cards.forEach((card, index) => {
+                    card.style.filter = index === cardIndex ? "none" : "blur(5px)"
+                    card.classList.toggle("atual", index === cardIndex)
+                    
+                })
+        
+      
       atualizarCarrossel()
     }
   
@@ -16,13 +23,13 @@ document.addEventListener("DOMContentLoaded", function() {
       if (direcao === "prev" && cardIndex > 0) {
         cardIndex--;
       } else if (direcao === "next" && cardIndex < cards.length - 1) {
-        cardIndex++;
+        cardIndex++
       }
-      atualizarCarrossel();
+      atualizarCarrossel()
     }
   
-    document.getElementById("anterior").addEventListener("click", () => moverCarrossel("prev"));
-    document.getElementById("proximo").addEventListener("click", () => moverCarrossel("next"));
+    document.getElementById("anterior").addEventListener("click", () => moverCarrossel("prev"))
+    document.getElementById("proximo").addEventListener("click", () => moverCarrossel("next"))
   
-    atualizarCarrossel();
-  });
+    atualizarCarrossel()
+  })
